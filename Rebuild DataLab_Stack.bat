ab1059c8414caedb26f794c89d1b43896ff3685f
Müller <Mue@awk.ch>
2020-02-21 15:56:50 +0100

    Batch-Skripte hinzugefugt fur Rebuild, Start und Stop des DataLab_Stack

---
+++ Rebuild DataLab_Stack.bat
@@ -0,0 +1,3 @@
+docker rmi controller_controlboard
+
+docker-compose -f "C:/Users/mue/Desktop/DataLab_Development/docker-compose.yml" -p "controller" build 
---
+++ Stop DataLab_Stack.bat
@@ -0,0 +1,1 @@
+docker-compose -f "C:/Users/mue/Desktop/DataLab_Development/docker-compose.yml" -p "controller" down
