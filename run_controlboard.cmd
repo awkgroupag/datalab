8f13c585e51afbf510f2b35d753c4edeb7554583
ThomasKat <thomas.kathriner@awk.ch>
2020-04-27 17:15:52 +0200

    Fix Jupyter token not being picked up by run_controlboard.cmd

--- run_controlboard.cmd
+++ run_controlboard.cmd
@@ -1,20 +1,20 @@
 @echo off
 set PORT=12334
+set DOCKER_NETWORK=datalab-network
 
-docker network create datalab-network
+echo Starting controlboard
+docker network create %DOCKER_NETWORK% >nul 2>nul
 docker-compose -f controlboard.yml up -d
 
 echo Waiting for the controlboard to spin up on port %PORT%
 
-:loop
-timeout /t 1 /nobreak >nul 2>nul
-:: Beware: netstat output depends on Windows locale. Also, Umlaute are a pain
-netstat -nao | findstr "0:%PORT%" | findstr "LISTENING ABH" >nul 2>nul
-if not %ERRORLEVEL% equ 0 goto loop
-
+::
+:: Wait until Jupyter is really ready
+::
+set COUNTER=0
+:wait_for_token
 :: Wait for another second so the notebook is indeed listening
 timeout /t 1 /nobreak >nul 2>nul
-
 ::Grab the Jupyter token
 setlocal
 for /F "tokens=* USEBACKQ" %%F IN (`"docker logs controlboard 2>&1 | findstr "http://127.0.0.1:8888/?token=""`) DO (
@@ -25,6 +25,11 @@ for /F "tokens=* USEBACKQ" %%F IN (`"docker logs controlboard 2>&1 | findstr "ht
 for /F "tokens=1-2 delims==" %%i in ("%TOKENSTRING%") do (  
     set TOKEN=%%j
 )
+set /A COUNTER=COUNTER+1
+if "%TOKEN%" == "" if %COUNTER% LSS 30 goto wait_for_token
+:: Once counter is reached, we did not get a token
+if "%TOKEN%" == "" goto error_empty_token
+
 
 :: Start Chrome
 echo.
@@ -32,3 +37,12 @@ echo Use the following URL to access your controlboard:
 echo http://localhost:%PORT%/?token=%TOKEN%
 
 start chrome http://localhost:%PORT%/?token=%TOKEN%
+goto end_of_file
+
+
+:error_empty_token
+echo.
+echo ERROR: no token found for your Jupyter Notebook :-(
+echo.
+
+:end_of_file
