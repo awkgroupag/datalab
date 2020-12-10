b564e23aa285f2a2e18312481305cb90185f52c8
ThomasKat <thomas.kathriner@awk.ch>
2020-12-10 07:08:57 +0100

    Refactor changing of Windows codepage

--- rm_controlboard.cmd
+++ rm_controlboard.cmd
@@ -6,14 +6,6 @@ set APPNAME=Controlboard
 set ENVIRONMENT_FILE_PATH=.\datalab-stacks\environment.env
 set COMPOSE_FILE=controlboard.yml
 
-:: Switch the windows codepage to utf-8 to let us read files correctly
-:: Do this temporarily to not mess with other programs - safe the current value
-FOR /F "tokens=2 delims=:" %%i IN ('chcp') DO SET "CHCP_CURRENT=%%i"
-:: Get rid of the period. at the end
-SET CHCP_CURRENT=%CHCP_CURRENT:~0,-1%
-:: Change codepage to utf-8
-CHCP 65001 >nul
-
 if not exist %ENVIRONMENT_FILE_PATH% (
     echo ERROR: Environment file %ENVIRONMENT_FILE_PATH% not found!
     echo Please edit %ENVIRONMENT_FILE_PATH%.EXAMPLE and save it as %ENVIRONMENT_FILE_PATH%
@@ -22,15 +14,22 @@ if not exist %ENVIRONMENT_FILE_PATH% (
     goto end_of_file
 )
 
+:: Switch the windows codepage to utf-8 to let us read files correctly
+:: Do this temporarily to not mess with other programs - safe the current value
+FOR /F "tokens=2 delims=:" %%i IN ('chcp') DO SET "CHCP_CURRENT=%%i"
+:: Get rid of the period. at the end
+SET CHCP_CURRENT=%CHCP_CURRENT:~0,-1%
+:: Change codepage to utf-8
+CHCP 65001 >nul
 
 :: Need to set the Windows environment variables from a dedicated environment
 :: file. The environmnent variables will be used/referenced within the
 :: docker-compose.yml
 :: Set environment variables from file. Skip lines starting with #
 FOR /F "tokens=*" %%i in ('findstr /v /c:"#" %ENVIRONMENT_FILE_PATH%') do SET %%i
-:: set Docker's environemtn variable COMPOSE_FILE - this way we can deal with
-:: SEVERAL docker-composes in ONE environment.env
 
+:: Switch codepage back
+chcp %CHCP_CURRENT% >nul
 
 docker-compose down
 
@@ -45,5 +44,3 @@ echo     remove container:        docker remove CONTAINER_ID
 echo.
 
 :end_of_file
-:: Switch codepage back
-chcp %CHCP_CURRENT% >nul
--- stop_jupyter_notebook.cmd
+++ stop_jupyter_notebook.cmd
@@ -5,14 +5,6 @@ setlocal
 set APPNAME=Jupyterlab Notebook
 set ENVIRONMENT_FILE_PATH=.\datalab-stacks\environment.env
 
-:: Switch the windows codepage to utf-8 to let us read files correctly
-:: Do this temporarily to not mess with other programs - safe the current value
-FOR /F "tokens=2 delims=:" %%i IN ('chcp') DO SET "CHCP_CURRENT=%%i"
-:: Get rid of the period. at the end
-SET CHCP_CURRENT=%CHCP_CURRENT:~0,-1%
-:: Change codepage to utf-8
-CHCP 65001 >nul
-
 if not exist %ENVIRONMENT_FILE_PATH% (
     echo ERROR: Environment file %ENVIRONMENT_FILE_PATH% not found!
     echo Please edit %ENVIRONMENT_FILE_PATH%.EXAMPLE and save it as %ENVIRONMENT_FILE_PATH%
@@ -21,11 +13,23 @@ if not exist %ENVIRONMENT_FILE_PATH% (
     goto end_of_file
 )
 
+:: Switch the windows codepage to utf-8 to let us read files correctly
+:: Do this temporarily to not mess with other programs - safe the current value
+FOR /F "tokens=2 delims=:" %%i IN ('chcp') DO SET "CHCP_CURRENT=%%i"
+:: Get rid of the period. at the end
+SET CHCP_CURRENT=%CHCP_CURRENT:~0,-1%
+:: Change codepage to utf-8
+CHCP 65001 >nul
+
 :: Need to set the Windows environment variables from a dedicated environment
 :: file. The environmnent variables will be used/referenced within the
 :: docker-compose.yml
 :: Set environment variables from file. Skip lines starting with #
 FOR /F "tokens=*" %%i in ('findstr /v /c:"#" %ENVIRONMENT_FILE_PATH%') do SET %%i
+
+:: Switch codepage back
+chcp %CHCP_CURRENT% >nul
+
 :: set Docker's environemtn variable COMPOSE_FILE - this way we can deal with
 :: SEVERAL docker-composes in ONE environment.env
 set COMPOSE_FILE=.\datalab-stacks\%DATALAB_JUPYTER_COMPOSE_PATH%
@@ -37,5 +41,3 @@ echo %APPNAME% container has been shut down
 echo The container's data is retained for the next start of the container
 
 :end_of_file
-:: Switch codepage back
-chcp %CHCP_CURRENT% >nul
